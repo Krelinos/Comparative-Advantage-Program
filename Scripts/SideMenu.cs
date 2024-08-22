8cf6ba2a6d0b0ab45e949d44f76ea8a65ba917ed
Krelinos <emmer.palaming@gmail.com>
2024-08-22 17:46:04 -0500

    SideMenu functionality things

---
+++ Scripts/SideMenu.cs
@@ -0,0 +1,100 @@
+using Godot;
+using System;
+
+namespace ComparativeAdvantage {
+
+public class SideMenu : CanvasLayer
+{
+    [Signal] protected delegate void OffscreenWindowVisibilityChanged( bool IsVisible );
+
+    [Export] protected NodePath __MenuButton;
+    [Export] protected NodePath __Backdrop;
+    [Export] protected NodePath __OffscreenWindow;
+    [Export] protected NodePath __OnscreenWindow;
+    [Export] protected NodePath OtherSideMenu;
+    [Export] protected readonly bool IsRightOriented;
+
+    public Button MenuButton
+    {
+        get { return _MenuButton; }
+        protected set { _MenuButton = value; }
+    }
+    
+    public Control Backdrop
+    {
+        get { return _Backdrop; }
+        protected set { _Backdrop = value; }
+    }
+    
+    public Container OffscreenWindow
+    {
+        get { return _OffscreenWindow; }
+        protected set { _OffscreenWindow = value; }
+    }
+    
+    public Container OnscreenWindow
+    {
+        get { return _OnscreenWindow; }
+        protected set { _OnscreenWindow = value; }
+    }
+
+    private Button _MenuButton;
+    private Control _Backdrop;
+    private Container _OffscreenWindow;
+    private Container _OnscreenWindow;
+
+    protected bool IsMenuOpen;
+
+    public override void _Ready()
+    {
+        MenuButton = GetNode<Button>( __MenuButton );
+        Backdrop = GetNode<Control>( __Backdrop );
+        OffscreenWindow = GetNode<Container>( __OffscreenWindow );
+        OnscreenWindow = GetNode<Container>( __OnscreenWindow );
+
+        MenuButton.Connect( "pressed", this, nameof(ToggleMenu) );
+        GetNode<SideMenu>( OtherSideMenu ).
+    }
+
+    protected void ToggleMenu()
+    {
+        IsMenuOpen = !IsMenuOpen;
+        EmitSignal( nameof(OffscreenWindowVisibilityChanged), IsMenuOpen );
+
+        if ( IsMenuOpen )
+        {
+            Tween tween = new Tween();
+            tween.InterpolateProperty(this, "offset:x", OffscreenWindow.RectSize.x * (IsRightOriented ? 1 : -1), 0, 1f, Tween.TransitionType.Quad, Tween.EaseType.Out );
+            AddChild(tween);
+            tween.Start();
+
+            Tween tween2 = new Tween();
+            tween2.InterpolateProperty(Backdrop, "modulate", new Color(1,1,1,0f), new Color(1,1,1,0.5f), 1f, Tween.TransitionType.Quad, Tween.EaseType.Out );
+            AddChild(tween2);
+            tween2.Start();
+
+            Backdrop.MouseFilter = Control.MouseFilterEnum.Stop;
+        }
+        else
+        {
+            Tween tween = new Tween();
+            tween.InterpolateProperty(this, "offset:x", 0, OffscreenWindow.RectSize.x * (IsRightOriented ? 1 : -1), 1f, Tween.TransitionType.Quad, Tween.EaseType.Out );
+            AddChild(tween);
+            tween.Start();
+
+            Tween tween2 = new Tween();
+            tween2.InterpolateProperty(Backdrop, "modulate", new Color(1,1,1,0.5f), new Color(1,1,1,0f), 1f, Tween.TransitionType.Quad, Tween.EaseType.Out );
+            AddChild(tween2);
+            tween2.Start();
+
+            Backdrop.MouseFilter = Control.MouseFilterEnum.Ignore;
+        }
+    }
+
+    protected void OnOtherMenuOffscreenWindowVisibilityChanged( bool IsVisible )
+    {
+
+    }
+}
+
+}
